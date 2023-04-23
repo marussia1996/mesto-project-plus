066e6ee9bc745e7e14cc95f06e0b451798cee220
marussia1996 <mariashirokova1996@gmail.com>
2023-04-23 14:26:07 +0400

    lint resolved

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -2,19 +2,18 @@ import { NextFunction, Request, Response } from 'express';
 import ErrorResponse from '../utils/errorResponse';
 
 const errorHandler = (err: ErrorResponse, req: Request, res:Response, next: NextFunction) => {
-  console.log(err.name)
   let error = { ...err };
   if (err.name === 'CastError') {
     const message = 'Пользователь/Карточка по указанному _id не найден';
     error = new ErrorResponse(message, 404);
   }
-  else if (err.name === 'ValidationError') {
+  if (err.name === 'ValidationError') {
     const message = 'Переданы некорректные данные при создании/обновлении пользователя';
     error = new ErrorResponse(message, 400);
   }
-  console.log(error.statusCode)
   res.status(error.statusCode || 500).json({
     message: error.message || 'Ошибка сервера',
   });
+  next();
 };
 export default errorHandler;
